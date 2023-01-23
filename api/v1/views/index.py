568e5b49e7f495376efb1ef47574cc3b62afa71d
robertokoba7 <okobarobert7@gmail.com>
2023-01-23 13:06:06 +0300

    [add] fix

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,33 +1,33 @@
 #!/usr/bin/python3
-"""index.py to connect to API"""
+"""Index page"""
+
 from api.v1.views import app_views
-from flask import Flask, Blueprint, jsonify
+from flask import jsonify
 from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 
-
-hbnbText = {
-    "amenities": "Amenity",
-    "cities": "City",
-    "places": "Place",
-    "reviews": "Review",
-    "states": "State",
-    "users": "User"
-}
+classes = [Amenity, City, Place, Review, State, User]
 
 
 @app_views.route('/status', strict_slashes=False)
-def hbnbStatus():
-    """hbnbStatus"""
-    return jsonify({"status": "OK"})
+def index_status():
+    """Index route"""
+    return jsonify(status="OK")
 
 
 @app_views.route('/stats', strict_slashes=False)
-def hbnbStats():
-    """hbnbStats"""
-    return_dict = {}
-    for key, value in hbnbText.items():
-        return_dict[key] = storage.count(value)
-    return jsonify(return_dict)
-
-if __name__ == "__main__":
-    pass
+def get_stats():
+    """Get status"""
+    return jsonify(
+        amenities=storage.count(Amenity),
+        cities=storage.count(City),
+        places=storage.count(Place),
+        reviews=storage.count(Review),
+        states=storage.count(State),
+        users=storage.count(User)
+            )
