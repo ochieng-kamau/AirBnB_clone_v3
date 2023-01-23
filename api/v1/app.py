ba1b3a72dafd0f24e590d55706b0fcbba5a1096f
robertokoba7 <okobarobert7@gmail.com>
2023-01-23 11:54:44 +0300

    [add]: fix

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,27 +1,29 @@
 #!/usr/bin/python3
 """app.py to connect to API"""
-import os
+from os import getenv 
 from models import storage
 from api.v1.views import app_views
-from flask import Flask, Blueprint, jsonify, make_response
+from flask import Flask, jsonify
 from flask_cors import CORS
 
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
-cors = CORS(app, resources={"/*": {"origins": "0.0.0.0"}})
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
+cors = CORS(app, resources={r"/api/v1/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
-def teardown_appcontext(code):
+def teardown_appcontext(exc):
     """teardown_appcontext"""
     storage.close()
 
 
 @app.errorhandler(404)
 def page_not_found(error):
-    return make_response(jsonify({'error': 'Not found'}), 404)
+    return jsonify(error= 'Not found'), 404
 
 if __name__ == "__main__":
-    app.run(host=os.getenv('HBNB_API_HOST', '0.0.0.0'),
-            port=int(os.getenv('HBNB_API_PORT', '5000')))
+    host = getenv('HBNB_API_HOST') or '0.0.0.0'
+    port = getenv('HBNB_API_PORT') or 5000
+    app.run(host=host, port=port, threaded=True)
